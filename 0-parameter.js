c6dfbebf50b2d58b222c4ecce29b3e97a0459a34
Kaylin3121 <kayloo.williams@gmail.com>
2019-08-05 09:36:42 -0400

    Create 0-parameter.js

---
+++ 0-parameter.js
@@ -0,0 +1,6 @@
+<body>
+Kaylins Surface-Pro:6-javascript_continued kaylin$ node 0-parameter.js holberton
+Here is the first passed parameter: holberton
+Kaylins Surface-Pro:6-javascript_continued sylvain$ node 0-parameter.js 98
+Here is the first passed parameter: 98
+Kaylins Surface-Pro:6-javascript_continued kaylin$
