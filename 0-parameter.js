ad968595b349e0e56269064d55192ec61faac6d9
Kaylin3121 <kayloo.williams@gmail.com>
2019-08-05 09:43:35 -0400

    Update 0-parameter.js

--- 0-parameter.js
+++ 0-parameter.js
@@ -4,3 +4,5 @@ Here is the first passed parameter: holberton
 Kaylins Surface-Pro:6-javascript_continued sylvain$ node 0-parameter.js 98
 Here is the first passed parameter: 98
 Kaylins Surface-Pro:6-javascript_continued kaylin$
+Here is the first passed parameter: [PARAMETER]
+<body> 
