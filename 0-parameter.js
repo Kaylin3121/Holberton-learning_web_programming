2071088cc63410537455c0249dbb2c03a4f80b44
Kaylin3121 <kayloo.williams@gmail.com>
2019-08-05 10:21:50 -0400

    Update 0-parameter.js

--- 0-parameter.js
+++ 0-parameter.js
@@ -5,4 +5,8 @@ Kaylins Surface-Pro:6-javascript_continued sylvain$ node 0-parameter.js 98
 Here is the first passed parameter: 98
 Kaylins Surface-Pro:6-javascript_continued kaylin$
 Here is the first passed parameter: [PARAMETER]
-<body> 
+
+// Store a value passed by the user to the script in the variable my_parameter. Note it takes the second element which corresponds to the first parameter. Third element would correspond to second parameter
+let my_parameter = process.argv[2];
+// Print the my_parameter variable
+console.log(my_parameter)
